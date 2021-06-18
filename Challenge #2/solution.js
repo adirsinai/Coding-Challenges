8ad199b87ebfb9a024869c4f315431efd5cb714a
adirsinai <36900949+adirsinai@users.noreply.github.com>
2021-06-18 09:44:15 +0300

    Create solution.js

---
+++ Challenge #2/solution.js
@@ -0,0 +1,6 @@
+//challenge #2
+if (markHigherBmi) {
+    console.log(`"Mark's BMI (${markBmi}) is higher than than john's (${johnBmi})`)
+} else {
+    console.log(`"john's BMI (${johnBmi}) is higher than than Mark's (${markBmi})`)
+}
